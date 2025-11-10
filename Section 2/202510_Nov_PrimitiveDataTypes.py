cceaed0b1496428f194a2e183c15bf48e3b33712
Shafilah AF <shafilah.af@gmail.com>
2025-11-10 11:28:26 +0700

    feat: add Band Name Generator and Primitive Data Types examples

---
+++ Section 2/202510_Nov_PrimitiveDataTypes.py
@@ -0,0 +1,7 @@
+# Subscripting Strings
+name = "Fila"
+print("Length of name:", len(name))  # Output: 4
+print("First character:", name[0])  # Output: F
+print("Second character:", name[1])  # Output: i
+print("Last character:", name[-1])  # Output: a
+print("Second last character:", name[-2])  # Output: l
